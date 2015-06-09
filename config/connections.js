a493e5b2b7942299a4f58969c77e3a75cc2b6081
ero5004 <eric.obeysekare@gmail.com>
2015-06-09 14:22:01 -0400

    change to postgres?

--- config/connections.js
+++ config/connections.js
@@ -72,12 +72,11 @@ module.exports.connections = {
   *                                                                          *
   *                                                                          *
   ***************************************************************************/
-  somePostgresqlServer: {
+  herokuPostgresqlServer: {
     adapter: 'sails-postgresql',
-    host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_POSTGRES_USER',
-    password: 'YOUR_POSTGRES_PASSWORD',
-    database: 'YOUR_POSTGRES_DB'
+    url: process.env.DATABASE_URL,
+	pool: false,
+	ssl: false
   }
 
 
--- config/models.js
+++ config/models.js
@@ -17,7 +17,7 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-   connection: 'herokuMysqlServer',
+   connection: 'herokuPostgresqlServer',
 
   /***************************************************************************
   *                                                                          *
